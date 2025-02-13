388a36dd6addd9ab4098e7e6d8a7d889e6d296a4
jneisen <joseph@neisen.xyz>
2025-02-13 17:03:35 -0500

    Added user input, re-addressed the variables, and created a constant (12)

--- date.s
+++ date.s
@@ -2,8 +2,12 @@
 .data
 month_array:	.word	31, 28, 31, 30, 31, 30, 31, 31, 30, 31
 		.word 	30, 31
-start_date:	.word	2, 12, 2025
-num_days:	.word	1145
+ask_day:	.asciiz "What is day of the month is it today?\n"
+ask_month:	.asciiz "What month are we in?\n"
+ask_year:	.asciiz "What year is it?\n"
+ask_num_days:	.asciiz "How many days forward?\n"
+
+TWELVE = 12
 
 # code time
 # Approach:
@@ -15,30 +19,55 @@ num_days:	.word	1145
 .text
 .globl main
 
-main:	la $s0 start_date		# s0: start_date
-	la $s1 month_array		# s1: month_array
-	la $s2 num_days			
-	lw $s2, ($s2)			# s2: num_days
-	lw $s3, ($s0)			# s3: month tracker
-	li $s4, 12			# s4 = 12
-	lw $s5, 8($s0)			# s5 will be the year tracker
+main:	
+	li $v0, 4			# ask for the current day
+	la $a0 ask_day
+	syscall
 
-	lw $t0, 4($s0)			# start_date day to $t0
+	li $v0, 5
+	syscall
+	move $s1, $v0
 
-	addi $t1, $s3, -1		# subtract one from month
+	li $v0, 4			# ask for the current month
+	la $a0 ask_month
+	syscall
+
+	li $v0, 5
+	syscall
+	move $s2, $v0
+
+	li $v0, 4			#ask for the current year
+	la $a0 ask_year
+	syscall
+
+	li $v0, 5
+	syscall
+	move $s3, $v0
+
+	li $v0, 4			#ask for the amount of days
+	la $a0 ask_num_days
+	syscall
+
+	li $v0, 5
+	syscall
+	move $s4, $v0
+					# s1 = current day, s2 = current month, s3 = current year, s4 = num_days
+	la $s0 month_array		# s0: month_array
+
+	addi $t1, $s2, -1		# subtract one from month
 	sll $t1, $t1, 2			# shift left 2, (mult by 4)
-	add $t1, $t1, $s1		# add to beginning address
+	add $t1, $t1, $s0		# add to beginning address
 	
 	lw $t2, ($t1)			# loads the current month into $t2
 	jal february
 
-	sub $t0, $t2, $t0		# days left in current month
-	sub $t3, $s2, $t0		# num days - days left	
+	sub $s1, $t2, $s1		# days left in current month
+	sub $t3, $s4, $s1		# num days - days left	
 
 	ble $t3, $0, print 		# if(num_days would be <= 0): print
 	
-	move $s2, $t3
-	addi $s3, $s3, 1
+	move $s4, $t3
+	addi $s2, $s2, 1
 	addi $t1, $t1, 4
 
 #------------------------------------------
@@ -46,29 +75,29 @@ loop:
 	lw $t2, ($t1)
 	jal february
 	
-	sub $t3, $s2, $t2		# if this month is too much, print
+	sub $t3, $s4, $t2		# if this month is too much, print
 	ble $t3, $0, print
 
-	move $s2, $t3			# otherwise, continue to next month
-	addi $s3, $s3, 1
+	move $s4, $t3			# otherwise, continue to next month
+	addi $s2, $s2, 1
 	addi $t1, $t1, 4
 
-	bgt $s3, $s4, reset
+	bgt $s2, TWELVE, reset
 	j loop
 
 #------------------------------------------
 reset:
-	li $s3, 1			# reset month
-	move $t1, $s1			# reset month index
-	addi $s5, $s5, 1		# increment year
+	li $s2, 1			# reset month
+	move $t1, $s0			# reset month index
+	addi $s3, $s3, 1		# increment year
 	j loop
 
 #------------------------------------------
 february:				# this function checks if we are in february and in a leap year
 	li $t9, 2
-	bne $s3, $t9, return		# if not february, go back
+	bne $s2, $t9, return		# if not february, go back
 	li $t9, 4
-	div $s5, $t9			# if not divisible by 4, go back
+	div $s3, $t9			# if not divisible by 4, go back
 	mfhi $t9
 	bne $t9, $0, return
 
@@ -78,7 +107,7 @@ return:
 	jr $ra
 #------------------------------------------
 print:	li $v0, 1
-	move $a0, $s3			# print month
+	move $a0, $s2			# print month
 	syscall
 
 	li $v0, 11
@@ -86,7 +115,7 @@ print:	li $v0, 1
 	syscall
 
 	li $v0, 1
-	move $a0, $s2			# print day
+	move $a0, $s4			# print day
 	syscall
 
 	li $v0, 11			#print '/'
@@ -94,7 +123,7 @@ print:	li $v0, 1
 	syscall
 
 	li $v0, 1			#print year
-	move $a0, $s5
+	move $a0, $s3
 	syscall
 
 	li $v0, 10			#exit
